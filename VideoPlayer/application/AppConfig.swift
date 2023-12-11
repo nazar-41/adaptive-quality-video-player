f02e548bbdfb74a4fb1dc76314185b5e55fcf99f
nazar-41 <nazarwelkakayew41@gmail.com>
2023-12-11 17:19:00 +0500

    add: add video

--- VideoPlayer/application/AppConfig.swift
+++ VideoPlayer/application/AppConfig.swift
@@ -11,7 +11,7 @@ struct AppConfig {
     
     static let serverPort: UInt = 8080
     
-    static let playlistUrl = URL(string: "https://bv-storage.belet.me/videos/UCEkd3Rj_MSv8v6aA0h1G4fg/KEInn8yLDqM/master.m3u8")!
+    static let playlistUrl = URL(string: "https://bv-storage.belet.me/videos/UC3wygMmk_Tzc4wfMdErNsjA/Y8fHpJG5dKc/master.m3u8")!
 
     //static let playlistUrl = URL(string: "https://bv-storage.belet.me/videos/UCV0qA-eDDICsRR9rPcnG7tw/5bId3N7QZec/master.m3u8")!
     //static let playlistUrl = URL(string: "https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8")!
